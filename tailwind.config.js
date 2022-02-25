a2ecd5e727fa7961edeb2ee61407299b94345cc9
Tron51226 <70756241+Tron51226@users.noreply.github.com>
2022-02-25 16:12:39 +0530

    Updated WIP Page
    "Work in progress" page has been updated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        bgAnim: "bgKf 8s infinite",
+      },
+      keyframes:{
+        bgKf:{
+          "0%":{
+            transform: "translate(0px,0px) scale(0.8)"
+          },
+          "25%":{
+            transform: "translate(-180px,90px) scale(1)"
+          },
+          "50%":{
+            transform: "translate(0px,0px) scale(1.5)"
+          },
+          "75%":{
+            transform: "translate(210px,-60px) scale(1)"
+          },
+          "100%":{
+            transform: "translate(0px,0px) scale(0.8)"
+          }
+        }
+      },
+      fontFamily:{
+        workSansFont: "'Work Sans', sans-serif;",
+      }
+    },
   },
   plugins: [],
 }
