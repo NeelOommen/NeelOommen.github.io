35651cd8346e9eb36c9067dfe7b564d556c22b81
Tron51226 <70756241+Tron51226@users.noreply.github.com>
2022-03-21 16:58:19 +0530

    About me page
    Added an about me page with basic information about myself.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       animation:{
         bgAnim: "bgKf 8s infinite",
+        squareBgAnim: "sqBgKf 8s infinite",
       },
       keyframes:{
         bgKf:{
@@ -11,16 +12,33 @@ module.exports = {
             transform: "translate(0px,0px) scale(0.8)"
           },
           "25%":{
-            transform: "translate(-180px,90px) scale(1)"
+            transform: "translate(-100%,90px) scale(1)"
           },
           "50%":{
             transform: "translate(0px,0px) scale(1.5)"
           },
           "75%":{
-            transform: "translate(210px,-60px) scale(1)"
+            transform: "translate(100%,-60px) scale(1)"
           },
           "100%":{
             transform: "translate(0px,0px) scale(0.8)"
+          }
+        },
+        sqBgKf:{
+          "0%":{
+            transform: "translate(0px,0px) scale(0.8) rotate(0deg)"
+          },
+          "25%":{
+            transform: "translate(-180px,90px) scale(1) rotate(90deg)"
+          },
+          "50%":{
+            transform: "translate(0px,0px) scale(1.5) rotate(180deg)"
+          },
+          "75%":{
+            transform: "translate(210px,-60px) scale(1) rotate(270deg)"
+          },
+          "100%":{
+            transform: "translate(0px,0px) scale(0.8) rotate(360deg)"
           }
         }
       },
