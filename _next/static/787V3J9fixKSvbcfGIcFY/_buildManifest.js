534abdd1b901064c4f61e3daea96c3ee4bad7d90
NeelOommen <70756241+NeelOommen@users.noreply.github.com>
2022-12-09 08:39:05 +0000

    Deploying to gh-page from @ NeelOommen/neeloommen.github.io@380a2cfaebd78189f5b166f5a0568ebe61b81580 🚀

--- _next/static/787V3J9fixKSvbcfGIcFY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b68db7d1913f5cf2.css","static/chunks/pages/index-8eb3580e4a9b0d2d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
